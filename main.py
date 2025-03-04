6d8f090f8674d3c1a8b36cddc5fd36fc48b06259
Tsunami014 (Max) <Tsunami014@duck.com>
2025-03-04 19:42:37 +1100

    Moved the drawInf func back into main.py (waste of my time) and made the genMesh not count ocean, making it look good at last!
    Yeah, moving the function was a big waste of time - I need them to be different in both files.
    But now the ocean looks much better!
    Next I'll add a better queueing system!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-from API import get_location, lat_lngTOxy, getInf, SZE
+from API import get_location, lat_lngTOxy, getPlaceInfo, getHeightInfo
 import pygame
+from threading import Thread
 
 z = 9
 lat, lng, bbx = get_location(input('Choose a city in Australia (blank for Sydney) > ') or 'Sydney')
@@ -10,6 +11,76 @@ minx, miny = lat_lngTOxy(bbx[0], bbx[2], minmaxZoom)
 maxx, maxy = lat_lngTOxy(bbx[1], bbx[3], minmaxZoom)
 x -= 1
 y -= 1
+
+placesInf = {}
+SZE = 512
+WHITES = [pygame.Surface((SZE, SZE)), pygame.Surface((SZE*2, SZE*2))]
+WHITES[0].fill((255, 50, 50))
+WHITES[1].fill((255, 50, 50))
+
+def drawInf(x, y, z):
+    try:
+        inf = getPlaceInfo(x, y, z)
+        heightsur = getHeightInfo(x, y, z)
+    except AssertionError:
+        placesInf[(x, y, z)] = WHITES
+        return
+    except Exception:
+        placesInf.pop((x, y, z))
+        return
+    inf.sort(key=lambda x: x['importance'])
+    sur = pygame.Surface((SZE, SZE), pygame.SRCALPHA)
+    
+    COL_D = {
+        'water': (10, 50, 255),
+        'earth': (10, 255, 50),
+        'places': (255, 50, 50),
+        'pois': (255, 50, 50),
+        'transit': (90, 60, 100),
+        'boundaries': (0, 0, 0),
+        'roads': (255, 255, 50),
+        'landuse': (155, 130, 10, 200),
+        'buildings': (125, 125, 125),
+        'other': (255, 50, 255)
+    }
+    WIDTH = 5
+    for shp in inf:
+        if shp['group'] == 'earth':
+            continue
+        if shp['group'] in COL_D:
+            col = COL_D[shp['group']]
+        else:
+            col = COL_D['other']
+        if shp['type'] == 'MultiPolygon':
+            for poly in shp['coords']:
+                pygame.draw.polygon(sur, col, [[i[0]*SZE, i[1]*SZE] for i in poly[0]])
+        elif shp['type'] == 'Polygon':
+            pygame.draw.polygon(sur, col, [[i[0]*SZE, i[1]*SZE] for i in shp['coords'][0]])
+        elif shp['type'] == 'LineString':
+            pygame.draw.lines(sur, col, False, [[i[0]*SZE, i[1]*SZE] for i in shp['coords']], WIDTH)
+        elif shp['type'] == 'MultiLineString':
+            for ln in shp['coords']:
+                pygame.draw.lines(sur, col, False, [[i[0]*SZE, i[1]*SZE] for i in ln], WIDTH)
+        elif shp['type'] == 'Point':
+            pygame.draw.circle(sur, col, (shp['coords'][0] * SZE, shp['coords'][1] * SZE), WIDTH)
+    
+    heightsur.blit(sur, (0, 0))
+    placesInf[(x, y, z)] = (heightsur, pygame.transform.scale2x(heightsur))
+
+def getInf(pos):
+    if pos not in placesInf:
+        placesInf[pos] = None
+        Thread(target=drawInf, args=pos, daemon=True).start()
+    
+    if placesInf[pos] is not None:
+        return placesInf[pos][0]
+    elif pos[2] > 1:
+        pos2 = (math.floor(pos[0]/2), math.floor(pos[1]/2), pos[2]-1)
+        if pos2 in placesInf and placesInf[pos2] is not None:
+            xoff2 = int((pos[0]/2)%1 == 0.5)
+            yoff2 = int((pos[1]/2)%1 == 0.5)
+            sect = placesInf[pos2][1].subsurface(xoff2*SZE, yoff2*SZE, SZE, SZE)
+            return sect
 
 pygame.init()
 WIN = pygame.display.set_mode((1200, 900))
@@ -51,7 +122,7 @@ while run:
     WIN.fill((255, 255, 255))
     for yoff in (1, 2, 0):
         for xoff in (1, 2, 0, 3):
-            sur = getInf(math.floor(x)+xoff, math.floor(y)+yoff, math.floor(z))
+            sur = getInf((math.floor(x)+xoff, math.floor(y)+yoff, math.floor(z)))
             if sur is not None:
                 WIN.blit(sur, (xoff*SZE-(x%1)*SZE, yoff*SZE-(y%1)*SZE))
 
--- main3d.py
+++ main3d.py
@@ -1,8 +1,9 @@
 import pygame
 from objs import Mesh, surfaceToTexture
-from API import get_location, lat_lngTOxy, getHeightInfo, getInf
+from API import get_location, lat_lngTOxy, getPlaceInfo, getHeightInfo
 from OpenGL.GL import *  # noqa: F403
 from OpenGL.GLU import gluPerspective
+from functools import lru_cache
 
 # Initialize Pygame and OpenGL
 pygame.init()
@@ -35,13 +36,57 @@ glLoadIdentity()
 z = 9
 lat, lng, bbx = get_location('Sydney')
 x, y = lat_lngTOxy(lat, lng, z)
+SZE = 512
+@lru_cache
 def genMesh(x, y, z):
-    map = getHeightInfo(x, y, z)
-    sur = getInf(x, y, z, True)
-    tx = surfaceToTexture(sur)
-    fact = (map.get_width()//CHUNKSZE, map.get_height()//CHUNKSZE)
+    inf = getPlaceInfo(x, y, z)
+    heightsur = getHeightInfo(x, y, z)
+    inf.sort(key=lambda x: x['importance'])
+    realHei = pygame.Surface((SZE, SZE))
+    realHei.blit(heightsur, (0, 0))
+    
+    COL_D = {
+        'water': (10, 50, 255),
+        'earth': (10, 255, 50),
+        'places': (255, 50, 50),
+        'pois': (255, 50, 50),
+        'transit': (90, 60, 100),
+        'boundaries': (0, 0, 0),
+        'roads': (255, 255, 50),
+        'landuse': (155, 130, 10),
+        'buildings': (125, 125, 125),
+        'other': (255, 50, 255)
+    }
+    WIDTH = 5
+    def drawShp(shp, sur, col):
+        if shp['type'] == 'MultiPolygon':
+            for poly in shp['coords']:
+                pygame.draw.polygon(sur, col, [[i[0]*SZE, i[1]*SZE] for i in poly[0]])
+        elif shp['type'] == 'Polygon':
+            pygame.draw.polygon(sur, col, [[i[0]*SZE, i[1]*SZE] for i in shp['coords'][0]])
+        elif shp['type'] == 'LineString':
+            pygame.draw.lines(sur, col, False, [[i[0]*SZE, i[1]*SZE] for i in shp['coords']], WIDTH)
+        elif shp['type'] == 'MultiLineString':
+            for ln in shp['coords']:
+                pygame.draw.lines(sur, col, False, [[i[0]*SZE, i[1]*SZE] for i in ln], WIDTH)
+        elif shp['type'] == 'Point':
+            pygame.draw.circle(sur, col, (shp['coords'][0] * SZE, shp['coords'][1] * SZE), WIDTH)
+    
+    for shp in inf:
+        if shp['group'] == 'water':
+            drawShp(shp, realHei, 0)
+        if shp['group'] == 'earth':
+            continue
+        if shp['group'] in COL_D:
+            col = COL_D[shp['group']]
+        else:
+            col = COL_D['other']
+        drawShp(shp, heightsur, col)
+
+    tx = surfaceToTexture(heightsur)
+    fact = (SZE//CHUNKSZE, SZE//CHUNKSZE)
     return Mesh((0, 0, 0), [
-        [map.get_at((x*fact[0], y*fact[1]))[1]/255*hei-hei for x in range(CHUNKSZE)] for y in range(CHUNKSZE)
+        [realHei.get_at((x*fact[0]+CHUNKSZE//2, SZE-(y*fact[1]+CHUNKSZE//2)))[1]/255*hei-hei for x in range(CHUNKSZE)] for y in range(CHUNKSZE)
     ], texture=tx)
 
 objs = [
